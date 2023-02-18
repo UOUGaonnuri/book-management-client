cb00650d003660ca94f4d06d64121578f3553092
JShin <jaljayo85@gmail.com>
2023-02-19 00:48:18 +0900

    fix: 메인페이지원상복구

--- src/MainPage.tsx
+++ src/MainPage.tsx
@@ -1,20 +1,7 @@
-import React, { useContext, useState } from 'react'
-import { useNavigate } from 'react-router-dom';
-import {}
-
-
 const MainPage = () => {
-    const values = useContext(UserContext);
-    const goReg = useNavigate();
-    const userName: (undefined|string) = values?.state.user.userName;
-    const logout = ()=>{
-        values?.actions.logout();
-        goReg("/member/login");
-    };
     return (
         <div>
-            <h1>{userName? `${userName}님 환영합니다` : `로그인 해주세요`}&nbsp;&nbsp;&nbsp;
-            <span onClick={logout}>{userName?"로그아웃":""}</span></h1>
+            <h1>메인페이지</h1>
         </div>
     )
 }
