3925ec9fb4d2e9259f6e993537bea11dc683a2f2
JShin <jaljayo85@gmail.com>
2023-02-21 00:05:34 +0900

    feat: 상세보기페이지 스타일구현1

--- src/pages/BookPage.tsx
+++ src/pages/BookPage.tsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import { useParams } from 'react-router'
+import Style from './style/BookPage.module.css'
+import BookImg from './youUgly.png'
 
 type Props = {}
 type BookIdParam = { id: string }
@@ -7,7 +9,16 @@ type BookIdParam = { id: string }
 const BookPage = (props: Props) => {
     const params = useParams<BookIdParam>();
     return (
-        <div>book ID : {params.id}</div>
+        <div>
+            <div className={Style.leftconstainer}>
+                <img className={Style.ImgStyle} src={BookImg}/>
+            </div>
+            <div className={Style.rightconstainer}>
+                <h2>제목입니다</h2>
+                <br />
+                <li>내용입니다~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~</li>
+            </div>
+        </div>
     )
 }
 
--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react'
 import { useNavigate } from 'react-router-dom'
 import axios from 'axios'
-import LoginStyle from '../LoginPage.module.css'
+import LoginStyle from './style/LoginPage.module.css'
 import { setRefreshToken, getCookieToken } from '../storage/RefreshToken'
 import { AnyAction, Dispatch } from 'redux'
 import { connect } from 'react-redux'
