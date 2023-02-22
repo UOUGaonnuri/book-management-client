83dc9a99b0c2a6fc6902a7805c71fedb9734f297
JShin <jaljayo85@gmail.com>
2023-02-22 17:41:29 +0900

    feat: 관리자 페이지 제작(권한별로 나눠서 페이지를 막는 것은 못함)

--- src/pages/MasterPage.tsx
+++ src/pages/MasterPage.tsx
@@ -1,11 +1,55 @@
-import React from 'react'
+import axios from "axios"
+import { useState, useEffect } from 'react'
+import { useNavigate } from 'react-router-dom';
 
-type Props = {}
+import BookImg from './youUgly.png'
 
-const MasterPage = (props: Props) => {
-  return (
-    <div>MasterPage</div>
-  )
+type bookType = {
+    id: number;
+    title: string;
+    author: string;
+    publisher: string;
+    content: string;
+    fileName: string;
+    fileUrl: string;
 }
 
-export default MasterPage
+
+const MasterPage = () => {
+    const [bookListData, setBookListData] = useState<Array<bookType>>([]);
+    const goReg = useNavigate();
+    const listFunc = async() => {
+        try{
+            const res = await axios.get("http://localhost:3000/books");
+            setBookListData(res.data);
+            console.log(res);
+        }
+        catch(e){
+            console.log(e);
+        }
+    };
+    useEffect(() => {
+        listFunc();
+    }, []);
+    const imgstyle = { width: 90, height: 80};
+    console.log(bookListData);
+    let bookList = bookListData.map((book: bookType) => {
+        return(
+            <div key={book.id} className="col-6 col-md-4 col-lg-3">
+                <img src={/*book.fileUrl*/BookImg} className="img-thumbnail" style={imgstyle} />
+                <br />
+                <h6>{book.title}<button>수정</button><button>삭제</button></h6>
+                <br />
+                <br />
+            </div>
+        );
+    });
+    return (
+        <div>
+            <h2 className="m-4">BOOK LIST<button>추가</button></h2>
+            <div className="row">{bookList}</div>
+        </div>
+    )
+}
+
+export default MasterPage;
